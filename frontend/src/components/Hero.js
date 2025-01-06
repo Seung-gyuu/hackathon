294aa1eef26c99ac80e8fd8aab5078e88f715411
Juyoung <ljy9407@gmail.com>
2025-01-05 17:06:33 -0700

    feat: Integrate JSON parsing and dynamic rendering for travel recommendations
    - Updated AI prompt to ensure JSON response structure for travel recommendations.
    - Implemented JSON parsing logic to convert AI response into structured objects.
    - Enhanced PlanResult and RandomPlanner components to dynamically render parsed data.
    - Refactored DestinationCard and ResultModal components to handle structured itinerary data.
    - Ensured seamless rendering of dynamic travel plans and itineraries in both modal and result pages.

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,5 +1,9 @@
 "use client";
 import { motion } from "framer-motion";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 import Image from "next/image";
 import Button from "./Button";
 import { Fugaz_One } from "next/font/google";
@@ -12,7 +16,7 @@ export default function Hero() {
     variant1: {
       x: 0,
       y: -150,
-      opacity: 0.3,
+      opacity: 0.5,
       transition: {
         duration: 1,
         ease: "easeInOut",
@@ -23,7 +27,7 @@ export default function Hero() {
       // y: [0, 50, -50], // 약간 위아래로 움직임
       y: [-150, -100, -180, -100, -150], // 아래 → 중간 → 위
       rotate: [0, 360], // 두 바퀴 회전
-      opacity: [0.3, 0.8, 1], // 투명도 변화
+      opacity: [0.5, 0.8], // 투명도 변화
       transition: {
         x: { duration: 1.5, ease: "easeInOut" },
         y: { duration: 1.5, ease: "easeInOut" },
@@ -36,14 +40,9 @@ export default function Hero() {
   };
 
   return (
-    // <div className="relative flex items-center justify-center w-full h-[calc(100vh-8rem)] overflow-hidden text-center container">
     <div className="relative flex flex-col items-center justify-center text-center h-[calc(100vh-8rem)] px-4 md:px-8 lg:px-16 ">
-      {/* <div className="absolute w-1/3 rounded-full opacity-40 left-28 top-1/3 h-1/4 bg-primary blur-3xl -z-10"></div> */}
-      {/* <div className="absolute w-1/3 rounded-full opacity-30 right-28 top-1/2 h-1/4 bg-secondary blur-3xl -z-10"></div> */}
-
       <div className="absolute bottom-0 w-full -translate-x-1/2 opacity-80 -translate-y-1/3 h-1/2 -z-10 left-1/2">
         <Image
-          // src="/images/travel-5.png"
           src="/images/travel-7.png"
           alt="Left Travel Image"
           fill
@@ -63,11 +62,10 @@ export default function Hero() {
           alt="Left Travel Image"
           fill
           objectFit="contain"
-          className="object-cover opacity-70"
+          className="object-cover "
         />
       </motion.div>
 
-      {/* mid contents */}
       <h1 className="mb-4 text-5xl font-bold md:text-6xl text-neutralDark">
         Your Next Stop, <span className="text-primary">Guided by AI</span>
       </h1>
@@ -80,19 +78,19 @@ export default function Hero() {
       <div className="flex gap-4 mb-12">
         <Link
           href="/planning"
-          className="px-8 py-3 text-lg text-white transition shadow-md rounded-3xl bg-primary hover:bg-primaryDark"
+          className="flex items-center gap-2 px-8 py-3 text-lg text-white transition shadow-md rounded-3xl bg-primary hover:bg-primaryDark"
         >
-          Plan My Trip
+          <IoIosArrowDropleftCircle size={24} />
+          <p>Plan it, Then Go!</p>
         </Link>
         <Link
           href="/random"
-          className="px-8 py-3 text-lg transition border-2 shadow-md rounded-3xl text-primary border-primary hover:bg-primary hover:text-white"
+          className="flex items-center gap-2 px-8 py-3 text-lg transition border-2 shadow-md rounded-3xl text-primary border-primary hover:bg-primary hover:text-white"
         >
-          Surprise Me
+          <p>No Plan, Just Go!</p>
+          <IoIosArrowDroprightCircle size={24} />
         </Link>
       </div>
     </div>
-    // </div>
-    // </div>
   );
 }
