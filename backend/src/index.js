c70ec4745e91b2bcc91e3671769249cfd10752f9
seung-gyuu <arnastasa7@gmail.com>
2025-01-05 21:47:38 -0700

    changed code

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL,
-      process.env.FRONTEND_URL_PROD
+      "http://localhost:3000", // 개발 환경
+      "hackathon-six-woad.vercel.app"
     ],
     methods: ["GET", "POST"],
     credentials: true,
